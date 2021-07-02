80b874c61c4bc009aca4f0c6cd82a99833206aaa
Techbrid-SARVESH-AHUJA <10026.dpsgv@dpsgs.org>
2021-07-02 20:06:04 +0530

    Initial Commit 20

--- app/models.py
+++ app/models.py
@@ -90,8 +90,6 @@ class basic_project(models.Model):
     image=models.ImageField(upload_to='static')
     video_link=models.CharField(max_length=300, null=True)
     about_1=models.TextField(max_length=500, null=True)
-    about_2=models.TextField(max_length=500, null=True)
-    about_3=models.TextField(max_length=500, null=True)
     def __str__ (self):
         return self.name
 

079d2464c564ee909a673de335658bb9526530c9
Techbrid-SARVESH-AHUJA <10026.dpsgv@dpsgs.org>
2021-08-15 19:27:31 +0530

    Initail Commit 30

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import *
 
+
+
+
+
 admin.site.register(site)
 admin.site.register(social_media_link)
 admin.site.register(blog_data)
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,10 @@ from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
 
+admin.site.site_header = "Techbrid Sarvesh Dashboard"
+admin.site.site_title = "Techbrid Sarvesh"
+admin.site.index_title = "ADMIN"
+
 urlpatterns = [
     
     path('admin/', admin.site.urls),
